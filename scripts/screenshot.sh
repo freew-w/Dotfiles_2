d70e9d8eb4f43a46ed0e3e41fc6f3b400efd7d66
free <freeadlau@gmail.com>
2025-11-10 00:32:29 +0800

    Add option to open in file manager

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -2,4 +2,9 @@
 filename="screenshot_$(date +%Y%m%d_%H%M%S).png"
 file_path="$HOME/Pictures/$filename"
 mkdir -p "$(dirname "$file_path")"
-grim -g "$(slurp)" "$file_path" && notify-send "Screenshot" "Saved to $file_path"
+if grim -g "$(slurp)" "$file_path"; then
+    response="$(notify-send --action="open=Open" "Screenshot" "Saved to $file_path")"
+    if [[ "$response" == "open" ]]; then
+        thunar "$(dirname "$file_path")"
+    fi
+fi
